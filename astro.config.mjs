15795585352e174ea422f9063cb30a3900857691
Ramiro Alegre <alegre.ramiro222@gmail.com>
2026-07-04 21:36:34 -0300

    change portfolio to astro

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ramiroalegre.com.ar',
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
